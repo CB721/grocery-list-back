6696c4e3b441ca3bfdb60d62f7212cd658c3607a
Clint Brodar <clintbrodar@gmail.com>
2020-04-05 16:36:11 -0700

    Cors

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
 const path = require("path");
 require('dotenv').config();
 const mongoose = require('mongoose');
@@ -6,12 +8,14 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(cors({
+  origin: ["https://g-list-cb.herokuapp.com"],
+  credentials: true
+}));
+app.use(session({ secret: process.env.sessionSecret, resave: true, saveUninitialized: true, cookie: { maxAge: 7200000 } }));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
 app.use(routes);
 
@@ -23,10 +27,6 @@ mongoose
     console.log(err);
   });
 mongoose.set('useFindAndModify', false);
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`API server is on port ${PORT}`);
