5f9b90e5734df0393f4136a2819caeb603dd289c
Clint Brodar <clintbrodar@gmail.com>
2020-04-05 18:14:13 -0700

    heroku cors session

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ const app = express();
 //   origin: ["http://localhost:3000"],
 //   credentials: true
 // }));
-// app.use(cors({
-//   origin: ["https://g-list-cb.herokuapp.com"],
-//   credentials: true
-// }));
-// app.use(session({ secret: process.env.sessionSecret, resave: true, saveUninitialized: true, cookie: { maxAge: 7200000 } }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({
+  origin: ["https://g-list-cb.herokuapp.com", "http://localhost:3000"],
+  credentials: true
+}));
+app.use(session({ secret: process.env.sessionSecret, resave: true, saveUninitialized: true, cookie: { maxAge: 7200000 } }));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
