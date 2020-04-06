db76e37c106cb0a3bf0a8f21df36878c5d531461
Clint Brodar <clintbrodar@gmail.com>
2020-04-05 18:21:33 -0700

    heroku updates

--- server.js
+++ server.js
@@ -8,20 +8,11 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// app.use(cors({
-//   origin: ["http://localhost:3000"],
-//   credentials: true
-// }));
 app.use(cors({
   origin: ["https://g-list-cb.herokuapp.com", "http://localhost:3000"],
   credentials: true
 }));
 app.use(session({ secret: process.env.sessionSecret, resave: true, saveUninitialized: true, cookie: { maxAge: 7200000 } }));
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
