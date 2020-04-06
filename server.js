e47a6effe13f8a3f907740a43b2de8ca7c67b0d2
Clint Brodar <clintbrodar@gmail.com>
2020-04-05 17:12:57 -0700

    http instead of https for localhost testing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors({
-  origin: ["https://localhost:3000"],
+  origin: ["http://localhost:3000"],
   credentials: true
 }));
 // app.use(cors({
