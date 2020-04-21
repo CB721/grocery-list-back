096ad423663fe19d58fdd8027cb17162abdbef7e
Clint Brodar <clintbrodar@gmail.com>
2020-04-21 08:35:38 -0700

    Text code schema

---
+++ schemas/text_codes.sql
@@ -0,0 +1,10 @@
+CREATE TABLE text_codes
+(
+	id INT NOT NULL AUTO_INCREMENT
+    , number VARCHAR(15)
+    , code CHAR(6)
+    , date_requested TIMESTAMP
+    , user_id VARCHAR(100)
+    , PRIMARY KEY(id)
+    , FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
